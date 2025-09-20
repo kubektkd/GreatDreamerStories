9a12f549a2d5b76e9416e25470aff95ba365ec9a
MICHNIEWICZ Jakub <jamichniewic@lyreco.com>
2025-09-20 18:24:23 +0200

    Make Game class as a facade for GameManager class

--- src/atomiccode/greatDreamerStories/Game.java
+++ src/atomiccode/greatDreamerStories/Game.java
@@ -1,10 +1,79 @@
 package atomiccode.greatDreamerStories;
 
-import atomiccode.cthulhuEngine.inputsOutputs.windowing.Window;
+import atomiccode.cthulhuEngine.engineMain.engine.EngineConfigs;
+import atomiccode.greatDreamerStories.gameManagement.GameManager;
 
+/**
+ * Game facade that provides a simplified interface to the game management system.
+ * This class acts as a facade for GameManager, hiding the complexity of the underlying engine.
+ */
 public class Game {
-
-    private Window window;
-    public String title;
+    
+    private GameManager gameManager;
+    private String title;
     private int width, height;
+    
+    /**
+     * Creates a new Game instance with the specified title and dimensions.
+     * 
+     * @param title The title of the game
+     * @param width The width of the game window
+     * @param height The height of the game window
+     */
+    public Game(String title, int width, int height) {
+        this.title = title;
+        this.width = width;
+        this.height = height;
+        this.gameManager = new GameManager();
+    }
+    
+    /**
+     * Initializes the game with the provided engine configuration.
+     * 
+     * @param configs The engine configuration to use
+     */
+    public void init(EngineConfigs configs) {
+        gameManager.init(configs);
+    }
+    
+    /**
+     * Starts the game loop.
+     */
+    public void start() {
+        gameManager.start();
+    }
+    
+    /**
+     * Stops the game loop.
+     */
+    public void stop() {
+        gameManager.stop();
+    }
+    
+    /**
+     * Gets the game title.
+     * 
+     * @return The game title
+     */
+    public String getTitle() {
+        return title;
+    }
+    
+    /**
+     * Gets the game width.
+     * 
+     * @return The game width
+     */
+    public int getWidth() {
+        return width;
+    }
+    
+    /**
+     * Gets the game height.
+     * 
+     * @return The game height
+     */
+    public int getHeight() {
+        return height;
+    }
 }
--- src/atomiccode/greatDreamerStories/Launcher.java
+++ src/atomiccode/greatDreamerStories/Launcher.java
@@ -1,18 +1,19 @@
 package atomiccode.greatDreamerStories;
 
 import atomiccode.greatDreamerStories.configs.EngineConfigs_Build;
-import atomiccode.greatDreamerStories.gameManagement.GameManager;
 
 public class Launcher {
 
     private static final String VERSION = "0.0.1";
+    private static final String GAME_TITLE = "Great Dreamer Stories";
+    private static final int GAME_WIDTH = 1280;
+    private static final int GAME_HEIGHT = 720;
 
     public static void main(String[] args) {
-//        Game game = new Game("Tiled Game", 640, 480);
-//        game.start();
-
-        GameManager game = new GameManager();
-
+        // Create game instance using the facade
+        Game game = new Game(GAME_TITLE, GAME_WIDTH, GAME_HEIGHT);
+        
+        // Initialize and start the game
         game.init(new EngineConfigs_Build(VERSION));
         game.start();
     }
